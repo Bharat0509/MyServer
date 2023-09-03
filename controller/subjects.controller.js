eabe15485188ace1978d87991c53eeb17116e41c
bharat <bhartbhammar3336@gmail.com>
2023-09-03 17:06:53 +0530

    new featuer

--- controller/subjects.controller.js
+++ controller/subjects.controller.js
@@ -1,5 +1,5 @@
 import Subject from "../models/subject.model.js";
-
+import Note from "../models/note.model.js";
 export const createSubject = async (req, res) => {
     await Subject.create(req.body);
     return res.status(200).json("OK");
@@ -10,5 +10,15 @@ export const allSubjects = async (req, res) => {
     return res.status(200).json({
         success: true,
         data: subjects,
+    });
+};
+export const getSubject = async (req, res) => {
+    const subjectInfo = await Subject.findById(req.params.id).populate({
+        path: "notes",
+        model: Note,
+    });
+    return res.status(200).json({
+        success: true,
+        data: subjectInfo,
     });
 };
--- routes/subjectsRoutes.js
+++ routes/subjectsRoutes.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
     allSubjects,
     createSubject,
+    getSubject,
 } from "../controller/subjects.controller.js";
 const router = express.Router();
 
 router.post("/", createSubject);
 
 router.get("/", allSubjects);
+router.get("/:id", getSubject);
 
 export default router;
